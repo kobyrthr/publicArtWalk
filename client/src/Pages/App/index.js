635eadf25c2079604fe28c5ca52c826ae0f05a1b
kobyrthr-dev <kobyrthr@gmail.com>
2022-05-13 18:58:08 -0400

    added styling for map color and size. sitll just one marker.

--- client/src/Pages/App/index.js
+++ client/src/Pages/App/index.js
@@ -1,8 +1,11 @@
-import React, { useRef,useEffect, useState } from 'react';
+import React, { useRef,useEffect, useState, useMemo } from 'react';
 /* eslint import/no-webpack-loader-syntax: off */
-import Map, {Marker} from 'react-map-gl';
+import Map, {Marker, Popup} from 'react-map-gl';
+import CITIES from '../../../src/data/cities.json'
 
 const MapApp =()=> {
+  const [showPopup, setShowPopup] = React.useState(true);
+  const [popupInfo, setPopupInfo] = useState(null);
   return(
 
     <div>
@@ -13,8 +16,8 @@ const MapApp =()=> {
         latitude: 38.909671,
         zoom: 14
       }}
-      style={{width: 800, height: 600}}
-      mapStyle="mapbox://styles/mapbox/streets-v9"
+      style={{width: '100vw', height: '100vh'}}
+      mapStyle="mapbox://styles/mapbox/light-v10"
       mapboxAccessToken={'pk.eyJ1Ijoia29ieXJ0aHIiLCJhIjoiY2oweTVwaDRqMDFhajJ3cGVnODllOG92cCJ9.Zz99tb4K4fFB3Bgs54C8rA'}
     >
       <Marker longitude={-77.034084} latitude={38.909671} color="red" />
